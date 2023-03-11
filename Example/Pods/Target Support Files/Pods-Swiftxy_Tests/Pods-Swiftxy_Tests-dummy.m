ba21c28f9addee56838f78ad4c46155ba7efb522
n.lyapustin <n.lyapustin@tinkoff.ru>
2023-03-11 10:58:31 +0300

    Initial commit

---
+++ Example/Pods/Target Support Files/Pods-Swiftxy_Tests/Pods-Swiftxy_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Swiftxy_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Swiftxy_Tests
+@end
---
+++ Example/Pods/Target Support Files/Swiftxy/Swiftxy-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Swiftxy : NSObject
+@end
+@implementation PodsDummy_Swiftxy
+@end
