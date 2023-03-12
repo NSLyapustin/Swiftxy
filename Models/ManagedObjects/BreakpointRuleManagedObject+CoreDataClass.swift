170a7e650666c1fb8c8cc2de6b253380085fcad1
n.lyapustin <n.lyapustin@tinkoff.ru>
2023-03-12 18:12:27 +0300

    added nsmanagedobject

---
+++ Models/ManagedObjects/BreakpointRuleManagedObject+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  BreakpointRuleManagedObject+CoreDataClass.swift
+//  
+//
+//  Created by n.lyapustin on 12.03.2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(BreakpointRuleManagedObject)
+public class BreakpointRuleManagedObject: NSManagedObject {
+
+}
---
+++ Models/ManagedObjects/BreakpointRuleManagedObject+CoreDataProperties.swift
@@ -0,0 +1,23 @@
+//
+//  BreakpointRuleManagedObject+CoreDataProperties.swift
+//  
+//
+//  Created by n.lyapustin on 12.03.2023.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension BreakpointRuleManagedObject {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<BreakpointRuleManagedObject> {
+        return NSFetchRequest<BreakpointRuleManagedObject>(entityName: "BreakpointRule")
+    }
+
+    @NSManaged public var id: UUID?
+    @NSManaged public var ruleName: String?
+    @NSManaged public var ruleTemplate: String?
+
+}
