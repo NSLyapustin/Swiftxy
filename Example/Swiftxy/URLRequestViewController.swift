f30e56b095c79326531fc4001dfc88ad9d4fc985
n.lyapustin <n.lyapustin@tinkoff.ru>
2023-06-18 22:38:32 +0300

    Update Example App

--- Example/Swiftxy/URLRequestViewController.swift
+++ Example/Swiftxy/URLRequestViewController.swift
@@ -6,6 +6,11 @@
 //  Copyright © 2023 CocoaPods. All rights reserved.
 //
 
+struct User: Codable {
+    let name: String
+    let age: String
+}
+
 import UIKit
 
 final class URLRequestViewController: UIViewController {
@@ -18,20 +23,23 @@ final class URLRequestViewController: UIViewController {
     }
 
     @IBAction func sendRequestButtonDidTapped(_ sender: Any) {
-        var components = URLComponents(string: "https://d3mj1.mocklab.io/json")!
+        var components = URLComponents(string: "http://proxyman.local:8002/process_json")!
         components.queryItems = [
             URLQueryItem(name: "example", value: "urlrequest")
         ]
 
-        let requestDto = RequestExample(id: 1, type: "Original")
+        let requestDto = RequestExample(id: 1, type: "Example")
 
         var request = URLRequest(url: components.url!)
         request.httpBody = try! JSONEncoder().encode(requestDto)
         request.httpMethod = "POST"
-        request.addValue("Original", forHTTPHeaderField: "type")
+        request.addValue("Example", forHTTPHeaderField: "type")
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")  // the request is JSON
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Accept")
 
         URLSession.proxied.dataTask(with: request) { data, response, error in
             guard let responseData = String(data: data!, encoding: .utf8) else { return }
+            print(responseData)
             DispatchQueue.main.async {
                 self.dataLabel.text = responseData
             }
